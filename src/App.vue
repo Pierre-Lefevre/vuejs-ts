2a3f03b6ae08a851d013b37d6ade0c047fd304d3
Pierre Lefèvre <lefevre.pierre.m.d@gmail.com>
2025-02-25 18:06:29 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { inject, ref } from "vue";
+import Demo from "./Demo.vue";
+const n = ref<"a">("a");
+const onDemo = (firstname: string, lastname: string) => {};
+inject<string>("demo");
+const onClick = (e: MouseEvent) => {};
+const div = ref<HTMLDivElement | null>(null);
+</script>
+
+<template>
+  <div ref="div" @click="onClick"><Demo @demo="onDemo" v-model="n" />Lorem</div>
+</template>
+
+<style scoped></style>
---
+++ src/Demo.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    prefix?: string;
+    person?: {
+      firstname: string;
+      lastname: string;
+    };
+  }>(),
+  {
+    prefix: "Bonjour",
+    // la func permet d'éviter d'avoir le même objet pour toutes les instances du cmpt
+    person: () => ({
+      firstname: "firstname",
+      lastname: "lastname",
+    }),
+  }
+);
+
+defineEmits<{
+  demo: [firstname: string, lastname: string];
+}>();
+
+defineModel<string>();
+</script>
+
+<template>
+  <p @click="$emit('demo', person.firstname, person.lastname)">
+    {{ prefix }} {{ person.firstname }}
+  </p>
+</template>
